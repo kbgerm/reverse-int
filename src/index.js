89d97bf2eba81359a48da1280e01d8776d0af29f
kb_germ <k.b.germ1806@gmail.com>
2023-09-01 00:58:29 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let a = Math.abs(n);
+  let nTos = a+""
+  let first = nTos[2]
+  let second = nTos[1]
+  let third = nTos[0]
+
+  let newString = first + second + third
+
+  let newN = Number(newString)
+
+  return newN
 }
